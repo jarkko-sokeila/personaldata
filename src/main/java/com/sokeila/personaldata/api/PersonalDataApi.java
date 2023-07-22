fcd25bd03ce3e7d91845c6a3a6277690bd0853e4
Jarkko Sokeila <jarkko.sokeila@gmail.com>
2023-07-22 13:18:30 +0300

    Implement OpenApi (swagger)

--- src/main/java/com/sokeila/personaldata/api/PersonalDataApi.java
+++ src/main/java/com/sokeila/personaldata/api/PersonalDataApi.java
@@ -3,8 +3,12 @@ package com.sokeila.personaldata.api;
 import com.sokeila.personaldata.model.Country;
 import com.sokeila.personaldata.model.Person;
 import com.sokeila.personaldata.services.PersonGenerator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,21 +27,25 @@ public class PersonalDataApi {
         this.personGenerator = personGenerator;
     }
 
-    @RequestMapping(value = "person", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get single person data",
+            parameters = {@Parameter(name = "country", allowEmptyValue = true, examples = {@ExampleObject("fi"), @ExampleObject("us")})})
+    @GetMapping(value = "person", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person getPerson(@RequestParam(required = false, name = "country") String country) {
         Country countryValue = Country.fromString(country);
 
         return personGenerator.generatePerson(countryValue);
     }
 
-    @RequestMapping(value = "persons", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get multiple persons. Count parameter must be between 1-1000.",
+            parameters = {@Parameter(name = "count"), @Parameter(name = "country", allowEmptyValue = true, examples = {@ExampleObject("fi"), @ExampleObject("us")})})
+    @GetMapping(value = "persons", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> getPersons(@RequestParam(name = "count") Integer count, @RequestParam(required = false, name = "country") String country) {
         List<Person> result = new ArrayList<>();
-        if(count < 0 || count > 1000) {
+        if (count < 0 || count > 1000) {
             return result;
         }
 
-        for(int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             Country countryValue = Country.fromString(country);
 
             result.add(personGenerator.generatePerson(countryValue));
